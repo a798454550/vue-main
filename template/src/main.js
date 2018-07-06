408a808f4a91732679e4b883a33ce2f8db49c3f7
ysj <946345582@qq.com>
2018-07-06 09:24:59 +0800

    删除内部模块引用

--- template/src/main.js
+++ template/src/main.js
@@ -12,20 +12,11 @@ import App from './App'
 import router {{#authentication}} , { routeAuthentication }  {{/authentication}} from '@/router'//注册路由配置
 
 import store from '@/store' //全局状态管理器
-import api from 'istrong-common-model';
 
 //region 使用 fastclick 解决移动端 300ms 点击延迟
 import FastClick from 'fastclick';
 FastClick.attach(document.body);
 //endregion 使用 fastclick 解决移动端 300ms 点击延迟
-
-Vue.use(api, {
-  apiConfig: {
-    // host: 'https://jsyapi.istrongcloud.com/api',//api的host
-    api: './config/api.json',
-    commonModel: './config/interface_common_model.json'
-  }
-});
 
 Vue.config.productionTip = false;
 
@@ -37,9 +28,7 @@ new Vue({
   template: '<App/>',
   components: { App },
   beforeCreate() {
-    this.$api.get({ url: './config/app.json', cache: true }).then(config => {
-      this.$store.commit('config/pushAppConfig', config || {});//配置信息初始化到config中
-    });
+
   },
   methods: {
   }
